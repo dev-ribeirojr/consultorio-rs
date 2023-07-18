28b8aa041a7fd6d0ff4190866f13a8fbbc03d42b
ribeiro-jr <ribeirojr1522@gmail.com>
2023-07-18 13:26:56 -0300

    :bug: corrigindo bug no component titulo ao logar pelo celular depois do deploy

--- src/components/titulo/index.js
+++ src/components/titulo/index.js
@@ -5,49 +5,53 @@ export default function Titulo({ texto }) {
 
     return (
         <section className="area-titulo">
-            <div
-                data-aos="fade-right"
-                data-aos-duration='1200'
-                className="border-left"
-            ></div>
-            <section className='titulo'>
-                <section
+            <section className='content-titulo'>
+                <div
                     data-aos="fade-right"
                     data-aos-duration='1200'
-                    data-aos-delay='200'
-                >
-                    <FaStarHalfAlt />
-                </section>
-                <section
-                    data-aos="fade-right"
-                    data-aos-duration='1200'
-                >
-                    <FaStarHalfAlt />
-                </section>
-                <h1
-                    data-aos="zoom-in"
-                    data-aos-duration='1200'
-                >{texto}</h1>
-                <section
-                    data-aos="fade-left"
-                    data-aos-duration='1200'
-                >
-                    <FaStarHalfAlt />
-                </section>
-                <section
-                    data-aos="fade-left"
-                    data-aos-duration='1200'
-                    data-aos-delay='200'
-                >
-                    <FaStarHalfAlt />
-                </section>
+                    className="border-left"
+                ></div>
+                <section className='titulo'>
+                    <section
+                        data-aos="fade-left"
+                        data-aos-duration='1200'
+                    >
+                        <FaStarHalfAlt />
+                    </section>
+                    <section
+                        data-aos="fade-left"
+                        data-aos-duration='1200'
+                        data-aos-delay='200'
+                    >
+                        <FaStarHalfAlt />
+                    </section>
+                    <h1
+                        data-aos="zoom-in"
+                        data-aos-duration='1200'
+                    >{texto}</h1>
+                    <section
+                        data-aos="fade-right"
+                        data-aos-duration='1200'
+                        data-aos-delay='200'
+                    >
+                        <FaStarHalfAlt />
+                    </section>
+                    <section
+                        data-aos="fade-right"
+                        data-aos-duration='1200'
+                    >
+                        <FaStarHalfAlt />
+                    </section>
 
+
+                </section>
+                <div
+                    data-aos="fade-left"
+                    data-aos-duration='1200'
+                    className="border-right"
+                ></div>
             </section>
-            <div
-                data-aos="fade-left"
-                data-aos-duration='1200'
-                className="border-right"
-            ></div>
+
         </section>
     )
 }
